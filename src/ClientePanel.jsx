c0797ca90894f761504e4f76a667029f132adeec
bluesbarber <jaramaximofarid@gmail.com>
2026-06-02 01:53:37 -0300

    Sacar realtime supabase, volver a version estable

--- src/ClientePanel.jsx
+++ src/ClientePanel.jsx
@@ -82,24 +82,7 @@ export default function ClientePanel({ cliente: clienteInicial, onLogout }) {
   useEffect(() => {
     cargarVisitasYPremio()
     animarPuntos(clienteInicial.puntos_actuales)
-
-    const canal = supabase
-      .channel('cliente-' + clienteInicial.id)
-      .on('postgres_changes', {
-        event: 'UPDATE',
-        schema: 'public',
-        table: 'cliente',
-        filter: 'id=eq.' + clienteInicial.id
-      }, payload => {
-        const nuevo = payload.new
-        setCliente(nuevo)
-        animarPuntos(nuevo.puntos_actuales)
-        cargarVisitasYPremio(nuevo.puntos_actuales)
-        cargarHistorial()
-      })
-      .subscribe()
-
-    return () => { cancelAnimationFrame(animRef.current); supabase.removeChannel(canal) }
+    return () => cancelAnimationFrame(animRef.current)
   }, [])
 
   async function canjearPremio(premio) {
